9d6b98bcba84d18744ea5c189b7d55b8a490da10
afzalhossain00 <afzalhossain.workspace@gmail.com>
2022-10-05 09:14:30 +0600

    fix cart items & calculations

--- src/component/Shop/Shop.js
+++ src/component/Shop/Shop.js
@@ -17,29 +17,37 @@ const Shop = () => {
             .then(data => setProducts(data))
     }, []);
 
-
     useEffect(() => {
-        const storedCart = getStoredCart()
+        const storedCart = getStoredCart();
         const savedCart = []
         for (const id in storedCart) {
             const addedProduct = products.find(product => product.id === id);
             if (addedProduct) {
-                const quantity = storedCart[id]
-                addedProduct.quantity = quantity
+                const quantity = storedCart[id];
+                addedProduct.quantity = quantity;
                 savedCart.push(addedProduct)
             }
         }
-
         setCart(savedCart);
-
     }, [products])
 
-    const handleAddToCart = (product) => {
-        // console.log(product);
+    const handleAddToCart = (selectedProduct) => {
+        console.log(selectedProduct);
+        let newCart = [];
+        const exists = cart.find(product => product.id === selectedProduct.id)
+        if (!exists) {
+            selectedProduct.quantity = 1;
+            newCart = [...cart, selectedProduct]
+        }
+        else {
+            const rest = cart.filter(product => product.id !== selectedProduct.id)
+            exists.quantity = exists.quantity + 1;
+            newCart = [...rest, exists]
+        }
         // Do not do this:  cart.push(product)
-        const newCart = [...cart, product]
+        // const newCart = [...cart, selectedProduct]
         setCart(newCart)
-        addToDb(product.id)
+        addToDb(selectedProduct.id)
     }
 
     return (
